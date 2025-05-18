09a103b417afcd8fbf1cfa5454f46b258a81a1bf
Lirihiwa <144426054+Lirihiwa@users.noreply.github.com>
2025-05-18 09:55:14 +0500

    lab 6.1

--- src/main/java/com/example/accounts/AccountService.java
+++ src/main/java/com/example/accounts/AccountService.java
@@ -1,10 +1,12 @@
 package com.example.accounts;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.*;
 
 public class AccountService {
-    private static final Map<String, UserProfile> users = new HashMap<>();
+
+    String URL = "jdbc:mysql://localhost:3306/mydb";
+    String USER = "root";
+    String PASSWORD = "123454321";
 
     public boolean registerUser(String username, String password, String email) {
         if(username == null
@@ -12,27 +14,57 @@ public class AccountService {
                 || password == null
                 || password.trim().isEmpty()
                 || email == null
-                || email.trim().isEmpty()
-                || users.containsKey(username)) {
+                || email.trim().isEmpty()) {
             return false;
         }
 
-        UserProfile newUserProfile = new UserProfile(username, password, email);
-        users.put(username, newUserProfile);
-        return true;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+                String sql = "INSERT IGNORE INTO users (username, password, email) VALUES (?, ?, ?)";
+
+                try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                    preparedStatement.setString(1, username);
+                    preparedStatement.setString(2, password);
+                    preparedStatement.setString(3, email);
+
+                    int rows = preparedStatement.executeUpdate();
+
+                    return rows >= 0;
+                }
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public UserProfile loginUser(String username, String password) {
-        UserProfile userProfile = users.get(username);
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
 
-        if(userProfile != null && userProfile.getPassword().equals(password)) {
-            return userProfile;
+            try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+                String sql = "SELECT username, password, email FROM users WHERE username = ? AND password = ?";
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+                preparedStatement.setString(1, username);
+                preparedStatement.setString(2, password);
+
+                ResultSet result = preparedStatement.executeQuery();
+
+                if (result.next()) {
+                    return new UserProfile(
+                            result.getString("username"),
+                            result.getString("password"),
+                            result.getString("email")
+                    );
+                }
+
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
         }
 
         return null;
-    }
-
-    public Map<String, UserProfile> getProfiles() {
-        return users;
     }
 }
